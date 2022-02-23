7610a9196e75660070c4ea8f5f9cc2ce2288976c
Karel Bašta <bastakka@email.cz>
2022-02-23 16:21:36 +0100

    Add nextcloud config

---
+++ Thesis/Jeff/Konfigurační soubory/nextcloud/config/config.php
@@ -0,0 +1,27 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'occp3yqbvmwe',
+  'passwordsalt' => 'DZAFDsl4gG9OZg8gacwZ2/0BPJEB4V',
+  'secret' => 'qFAJhw5imGgkRML33pMjXFWnGgrpx1sRbYHFsAknyCTT0lLW',
+  'trusted_domains' =>
+  array (
+    0 => 'jeff.sspbrno.cz',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '23.0.2.1',
+  'overwrite.cli.url' => 'https://jeff.sspbrno.cz/nextcloud',
+  'default_phone_region' => 'CS',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'gfMa2qZ44ewHyhpP',
+  'installed' => true,
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'maintenance' => false,
+  "default_language" => "cs",
+  "force_language" => "cs",
+);
